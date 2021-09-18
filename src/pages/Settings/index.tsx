96f748fde66bf123c5ff7522627b5d48ddbd8aa9
Web Sm@rt Dre@m <webdream1108@gmail.com>
2021-09-18 03:31:03 -0700

    feat:add setting page

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,13 +1,44 @@
 import React from 'react'
-// import styled from 'styled-components'
-import { WhiteCard } from '../../styledComponents/elements/Cards'
 
+import useImg from '../../assets/images/Ellipse 1.png'
+import { WhiteCard } from '../../styledComponents/elements/Cards'
 import { PageContainter } from '../../styledComponents/layout/StyledMainLayout'
+import {
+  AvatarDiv,
+  AvatarImage,
+  UserName,
+  UserDescription,
+  CardDiv,
+  AvatarBadge,
+  EditableInput,
+} from '../../styledComponents/pages/settings/StyledSetting'
+import { PencilSVG } from '../../components/CustomSVG'
 
 const Settings: React.FC = () => {
   return (
     <PageContainter>
-      <WhiteCard>adsf</WhiteCard>
+      <AvatarDiv>
+        <div>
+          <AvatarImage src={useImg} alt="avatar_image" />
+          <AvatarBadge>
+            <PencilSVG />
+          </AvatarBadge>
+        </div>
+        <UserName>User</UserName>
+        <UserDescription>New User</UserDescription>
+      </AvatarDiv>
+      <WhiteCard>
+        <EditableInput
+          value="adsf"
+          onChange={() => {
+            console.log('asdfasdf')
+          }}
+        />
+      </WhiteCard>
+      <CardDiv>
+        <WhiteCard>adsf</WhiteCard>
+        <WhiteCard>adsf</WhiteCard>
+      </CardDiv>
     </PageContainter>
   )
 }
---
+++ src/styledComponents/pages/settings/StyledSetting.tsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components'
+
+export const AvatarDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 40px;
+`
+export const AvatarBadge = styled.div`
+  background-color: #fff;
+  border-radius: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 28px;
+  height: 28px;
+  cursor: pointer;
+`
+
+export const AvatarImage = styled.img`
+  width: 169px;
+  height: 171px;
+`
+export const UserName = styled.span`
+  margin-top: 22px;
+  font-size: 20px;
+  font-weight: bold;
+`
+export const UserDescription = styled.span`
+  font-size: 16px;
+  color: #a2a2c2;
+`
+export const CardDiv = styled.div`
+  display: grid;
+  grid-template-columns: 49% 49%;
+  grid-gap: 2%;
+`
+export const EditableInput = styled.input`
+  border: none;
+  padding: 15px 15px;
+  border-radius: 10px;
+  font-size: 17px;
+  font-weight: 500;
+  background-color: #e5e9fa;
+  outline: none;
+  width: 100%;
+`
